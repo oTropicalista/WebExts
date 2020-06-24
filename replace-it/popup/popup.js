c6f98010807d7a97f3e763eba08245bcfe8fe72f
oTropicalista <nietzscheestamorto@gmail.com>
2020-06-23 21:53:50 -0300

    TabsManager and Replace It creates

---
+++ replace-it/popup/popup.js
@@ -0,0 +1,7 @@
+document.querySelector("input").focus();
+
+document.getElementById("btn_replace").onclick = function () { 
+    let tira = document.getElementById("var1").value;
+    let coloca = document.getElementById("var2").value;
+    alert(tira);
+};
